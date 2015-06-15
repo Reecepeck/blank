a120c300835b32db8b30b9cea2ae28abca53a45e
reecepeck <reecepeck1@gmail.com>
2015-06-15 14:25:07 +0100

    making repo
    1st commit

---
+++ git repo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gitrepo
+{
+    class Program
+    {
+        //Hello1.cs
+        public class Hello1
+        {
+            public static int Main(string[] args)
+            {
+                System.Console.WriteLine("Hello,World!");
+                var name = Console.ReadLine();
+                return 0;
+                //change 1
+            }
+
+        }
+    }
+}
---
+++ gitrepo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitrepo
+{
+    class Program
+    {
+//Hello1.cs
+        public class Hello1
+        {
+            public static int Main(string[] args)
+            {
+                System.Console.WriteLine("Hello,World!");
+                var name = Console.ReadLine();                
+                return 0;
+                //change 1
+        }
+        
+        }
+    }
+}
